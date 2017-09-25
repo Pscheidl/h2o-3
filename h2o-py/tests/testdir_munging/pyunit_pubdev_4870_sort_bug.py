ccc8af2f10fe10d46ba12178e97bf7ec315d378f
wendycwong <wendycwong@yahoo.com>
2017-09-25 11:23:57 -0700

    Final pyunit test.

--- h2o-py/tests/testdir_munging/pyunit_pubdev_4870_sort_bug.py
+++ h2o-py/tests/testdir_munging/pyunit_pubdev_4870_sort_bug.py
@@ -6,21 +6,9 @@ from tests import pyunit_utils
 
 def sort():
     df = h2o.import_file(pyunit_utils.locate("smalldata/synthetic/smallIntFloats.csv.zip"))
-    #df = h2o.H2OFrame(python_obj=list(range(0,250000000)))
-    #df = h2o.import_file(pyunit_utils.locate("bigdata/laptop/jira/twoColumns.csv"))
-   #
-   # df = h2o.import_file("hdfs://mr-0xd6/datasets/twoColumnsID.csv")
-   # df2 = h2o.import_file("hdfs://mr-0xd6/datasets/sorted2ColumnsID.csv")
-
-    df = h2o.import_file(pyunit_utils.locate("smalldata/synthetic/smallIntFloats.csv.zip"))
-    df = df.rbind(df).rbind(df).rbind(df).rbind(df).rbind(df).rbind(df).rbind(df).rbind(df).rbind(df).rbind(df).rbind(df).rbind(df)
-    df = df.rbind(df)
     sorted_column_indices = [0,1]
     df1 = df.sort(sorted_column_indices).asnumeric()
-    assert df1[0,0] <= df1[1,0]
-    h2o.remove_all()
-
- #   pyunit_utils.check_sorted_2_columns(df1, sorted_column_indices, prob=0.01)
+    pyunit_utils.check_sorted_2_columns(df1, sorted_column_indices, prob=0.05) # check some rows
 
 if __name__ == "__main__":
     pyunit_utils.standalone_test(sort)
